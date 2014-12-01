0f2411eabe06e34d530b69311b8a044b533e419d
Caden Lovelace <caden@herostrat.us>
2014-12-01 22:01:14 +0000

    Clean up code.

--- Gemfile
+++ Gemfile
@@ -1,9 +1,4 @@
 source 'https://rubygems.org'
 
-gem 'treat'
-gem 'stanford-core-nlp'
-gem 'scalpel'
-gem 'engtagger'
-
 gem 'pry'
 gem 'pry-byebug'
--- lib/brown_parser.rb
+++ lib/brown_parser.rb
@@ -2,7 +2,15 @@ class BrownParser
   attr_reader :words
 
   def self.news_corporea
-    %w{corporea/ca01 corporea/ca02 corporea/ca03 corporea/ca04 corporea/ca05 corporea/ca06 corporea/ca07 corporea/ca08 corporea/ca09 corporea/ca10 corporea/ca11 corporea/ca12 corporea/ca13 corporea/ca14 corporea/ca15 corporea/ca16 corporea/ca17 corporea/ca18 corporea/ca19 corporea/ca20 corporea/ca21 corporea/ca22 corporea/ca23 corporea/ca24 corporea/ca25 corporea/ca26 corporea/ca27 corporea/ca28 corporea/ca29 corporea/ca30 corporea/ca31 corporea/ca32 corporea/ca33 corporea/ca34 corporea/ca35 corporea/ca36 corporea/ca37 corporea/ca38 corporea/ca39 corporea/ca40 corporea/ca41 corporea/ca42 corporea/ca43 corporea/ca44}
+    %w{corporea/ca01 corporea/ca02 corporea/ca03 corporea/ca04 corporea/ca05
+       corporea/ca06 corporea/ca07 corporea/ca08 corporea/ca09 corporea/ca10
+       corporea/ca11 corporea/ca12 corporea/ca13 corporea/ca14 corporea/ca15
+       corporea/ca16 corporea/ca17 corporea/ca18 corporea/ca19 corporea/ca20
+       corporea/ca21 corporea/ca22 corporea/ca23 corporea/ca24 corporea/ca25
+       corporea/ca26 corporea/ca27 corporea/ca28 corporea/ca29 corporea/ca30
+       corporea/ca31 corporea/ca32 corporea/ca33 corporea/ca34 corporea/ca35
+       corporea/ca36 corporea/ca37 corporea/ca38 corporea/ca39 corporea/ca40
+       corporea/ca41 corporea/ca42 corporea/ca43 corporea/ca44 }
   end
 
   def initialize
